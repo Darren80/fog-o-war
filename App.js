cb89f39278d52cc2c4b56420810cff065f7eb1eb
Darren Ohonba - Evans <we444465@gmail.com>
2023-02-17 08:57:11 +0000

    fog-o-war

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! A Change.</Text>
+      <MapView provider={PROVIDER_GOOGLE} style={styles.map} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
