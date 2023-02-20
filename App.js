c66933d9c124c7ea6b3edc581aae14da9247a99a
abigail-a-lee <aabigaillee@gmail.com>
2023-02-20 11:27:52 +0000

    fixed import useState

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { PROVIDER_GOOGLE, Polyline, Polygon, Geojson } from "react-native-maps";
