4229e15c931c7c07d65390b48debe21f5dfd90ed
Matt <82216191+Mburnand-tech@users.noreply.github.com>
2023-02-24 17:02:32 +0000

    working a bit more

--- signIn.js
+++ signIn.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
 
 const signIn = () => {
+
+const [value, onChangeText] = React.useState('')
     return (
-      <View>
-        <Text>sign In !!!</Text>
+      <View style={styles.textInput}>
+        <TextInput
+            editable
+            multiline
+            numberOfLines={1}
+            maxLength={40}
+            value={value}
+            //style={{padding:10}}
+            onChangeText={text => onChangeText(text)}
+        />
+        <TextInput style={styles.textInput} secureTextEntry={true}/>
       </View>
     );
 }
 
 // ...
+
+const styles = StyleSheet.create({
+    textInput: {height: 40,
+                margin: 12,
+                borderWidth: 1,
+                padding: -20,
+    }
+ })
 
 export default signIn;
