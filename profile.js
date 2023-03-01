e8350d889f7e362903e8ddeddba8ea569eb4157b
Leah <leahb_96@hotmail.co.uk>
2023-03-01 19:47:19 +0000

    fixed a couple errors - probs more though

--- profile.js
+++ profile.js
@@ -4,8 +4,7 @@ import { TextInput, Button, Surface, List, useTheme} from 'react-native-paper'
 import {userLogOut} from './APIs'
 
 
-const profile = ({props}) => {
-    const {loggedIn, setLoggedIn} = props;
+const profile = () => {
     const [profileURL, setProfileURL] = useState('https://www.google.com/search?q=dog&rlz=1C1CHBF_en-GBGB991GB991&sxsrf=AJOqlzUbfns4rj4KgJIHlZgFvX__FUTesA:1677347401935&tbm=isch&source=iu&ictx=1&vet=1&fir=NN1-QGCky_XgzM%252CvL0suvKfyYaqHM%252C%252Fm%252F0bt9lr%253BOyQGKst6Lara3M%252CDj243kHCP9nJ-M%252C_%253BeQsdEWaZ6MHrQM%252C2prjuOFdTo6X8M%252C_%253BGbsqgMoKCwHzMM%252C65_J7MmDboKEcM%252C_%253BLdkDcR8aYOxQXM%252CsmcHwKUZvdWsaM%252C_&usg=AI4_-kQerRqCtAwxi4UQGOMadH6_xZ-OOQ&sa=X&ved=2ahUKEwjjqLqAnrH9AhWVFcAKHRx_BwYQ_B16BAh8EAE#imgrc=NN1-QGCky_XgzM')
     const [profileDisplayName, setDisplayName] = useState('Matt Burnand')
     const [profileUsername, setProfileUsername] = useState('Mburny_12star')
